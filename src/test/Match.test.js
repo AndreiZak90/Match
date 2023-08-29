84f6a3e5036c65fd9bdf8140366e479f0606bdef
AndreiZak <zak-90@inbox.ru>
2023-08-29 19:15:28 +0300

    two commit

--- src/test/Match.test.js
+++ src/test/Match.test.js
@@ -12,25 +12,25 @@ test("test 'stoned'", () => {
 
 test("test 'stoned'", () => {
   const main = new Main("magik");
-  main.powerValue = 90;
+  main.attackedValue = 90;
   main.distance = 2;
-  main.power = true;
+  main.stoneStatus = true;
   const result =
-    main.powerValue * (1 - (main.distance - 1) / 10) -
+    main.attackedValue * (1 - (main.distance - 1) / 10) -
     Math.log2(main.distance) * 5;
   expect(main.attack).toBe(result);
 });
 
 test("test 'stoned'", () => {
   const main = new Main("magik");
-  main.powerValue = 90;
+  main.attackedValue = 90;
   main.distance = 3;
-  main.power = false;
-  const result = main.powerValue * (1 - (main.distance - 1) / 10);
+  main.stoneStatus = false;
+  const result = main.attackedValue * (1 - (main.distance - 1) / 10);
   expect(main.attack).toBe(result);
 });
 
 test("test 'stoned'", () => {
   const main = new Main("magik");
-  expect((main.attack = 5)).toBe(main.powerValue);
+  expect((main.attack = 5)).toBe(main.attackedValue);
 });
